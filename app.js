7f72726118b8b5332dc594b719cba4c5141b6c9d
ISESSO <EBB0P16@jp.ibm.com>
2018-05-07 11:29:31 +0900

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ var appEnv = cfenv.getAppEnv();
 app.on('request', function(req, res){
     res.writeHead(200, {'Content-Type':'text/html'});
     res.write('<h1>Hello Node !</h1>');
-    res.end();
+    res.end('Hello World\n');
+//    res.end();
 });
 
 //add sto
